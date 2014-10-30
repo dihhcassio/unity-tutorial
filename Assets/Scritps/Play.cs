ef30b7b1bea2804ca5b99c9547710a0891141958
dihhcassio <adricarr@gmail.com>
2014-10-30 18:42:44 -0200

    Começo do projeto
    Criado arquivos iniciais do projeto.

---
+++ Assets/Scritps/Play.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play : MonoBehaviour {
+	
+	void OnMouseDown(){
+		Debug.Log ("Botao clicado");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Update");
+		
+	}
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Inciiou");
+	}
+}
